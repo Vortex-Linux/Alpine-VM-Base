254b65b19adee5db9bf261fa2c7fc1fc4ad8110d
arungeorgesaji <arungeorgesaji@gmail.com>
2024-09-29 22:40:16 +0530

    Basically a stopping commit,it uses some of the old code related to lvm partitioning though that part doesnt work yet

--- setup.sh
+++ setup.sh
@@ -44,7 +44,40 @@ setup-ntp -c chrony &&
 
 sed -i '/^#http:\/\/mirror.jingk.ai\/alpine\/v3.20\/community/s/^#//' /etc/apk/repositories && 
 apk update && 
-apk add sudo xorg-server xpra lvm2 
+apk add sudo xorg-server xpra lvm2 && 
+
+fdisk $DISK <<PARTITION
+
+g          
+n          
+p          
+1          
+
+
+w          
+PARTITION
+ 
+pvcreate ${DISK}1 && 
+vgcreate my_vg ${DISK}1 &&
+
+lvcreate --size 100P --thinpool my_thinpool my_vg &&
+
+lvcreate --thin --name my_root_lv --virtualsize 100P my_vg/my_thinpool && 
+lvcreate --thin --name my_boot_lv --virtualsize 100P my_vg/my_thinpool &&
+
+mkfs.ext4 /dev/my_vg/my_root_lv &&
+mkfs.ext4 /dev/my_vg/my_boot_lv &&
+
+mount /dev/my_vg/my_root_lv /mnt &&
+mkdir /mnt/boot &&
+mount /dev/my_vg/my_boot_lv /mnt/boot &&
+
+sed -i 's/^features=.*/features="ata base ide scsi usb virtio lvm"/' /mnt/etc/mkinitfs/mkinitfs.conf &&
+chroot /mnt mkinitfs &&
+
+chroot /mnt apk add grub &&
+chroot /mnt grub-install $DISK &&
+chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 EOF
 )
 
